e6774f5fc14f4a6c38964a00a49aff30a6741861
Austin Chayka <32969809+AustinChayka@users.noreply.github.com>
2022-05-25 19:34:32 -0700

    5.25.2022

--- dbConn.js
+++ dbConn.js
@@ -2,8 +2,8 @@ var mysql = require('mysql');
 var pool = mysql.createPool({
   connectionLimit : 10,
   host            : 'classmysql.engr.oregonstate.edu',
-  user            : '[user]',
-  password        : '[password]',
-  database        : '[dbname]'
+  user            : 'cs340_chaykaa',
+  password        : '6961',
+  database        : 'cs340_chaykaa'
 });
 module.exports.pool = pool;
--- main.js
+++ main.js
@@ -4,6 +4,9 @@ var bodyParser = require('body-parser');
 
 var mysql = require("./dbConn.js");
 
+var hbs = require("hbs");
+app.set("view engine", "hbs");
+
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 app.get('/', function (req, res) {
@@ -34,19 +37,16 @@ app.get('/view/', function (req, res) {
     res.sendFile(__dirname + '/view.html');
 })
 app.post('/viewResult/', urlencodedParser, function (req, res) {
-    result = {
-        table:req.body.table
-    };
-    console.log(result);
-    //query results and return
-})
+    var t = req.body.table;
+    mysql.pool.query('SELECT * FROM ' + t + ';', function (err, r, f) {
+        results = r;
+        console.log(results);
+        res.render("results", {results : results});
+    });
+});
 
-var server = app.listen(process.argv[2], 'localhost', function () {
+var server = app.listen(process.argv[2], 'localhost', function () { //rm localhost for server version
    var host = server.address().address
    var port = server.address().port
    console.log("Listening at http://%s:%s", host, port)
-})
-
-server.on("connection", function(client) {
-    console.log("New connection from: " + client.address().address);
 })
