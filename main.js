300424436aa2f1734e9e572f796f52e0d38f5515
Austin Chayka <32969809+AustinChayka@users.noreply.github.com>
2022-06-01 18:13:20 -0700

    6.1.2022

--- main.js
+++ main.js
@@ -40,11 +40,99 @@ app.post('/edit/addEntry/Animal/confirm', urlencodedParser, function (req, res) 
     });
     console.log(data)
 })
+app.post('/edit/addEntry/Plant', urlencodedParser, function (req, res) {
+    var t = req.body.table;
+    res.sendFile(__dirname + '/editPages/add' + t + '.html')
+})
+app.post('/edit/addEntry/Plant/confirm', urlencodedParser, function (req, res) {
+    data = {
+        commonName: req.body.CommonName == '' ? "NULL" : req.body.CommonName,
+        genus: req.body.Genus == '' ? "NULL" : req.body.Genus,
+        species: req.body.Species == '' ? "NULL" : req.body.Species,
+        subspecies: req.body.Subspecies == '' ? "NULL" : req.body.Subspecies
+    };
+    mysql.pool.query("INSERT INTO Plant (CommonName, Genus, Species, Subspecies) VALUES (?, ?, ?, ?);", [data.commonName, data.genus, data.species, data.subspecies], function (err, r) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        console.log(r.insertId);
+        res.sendFile(__dirname + '/confirm.html');
+    });
+    console.log(data)
+})
 app.get('/edit/remove/', function (req, res) {
     res.sendFile(__dirname + '/editPages/removeEntry.html');
 })
+app.post('/edit/removeEntry/', urlencodedParser, function (req, res) {
+    var t = req.body.table;
+    var query = 'SELECT * FROM ' + t + ';';
+    console.log(query);
+    mysql.pool.query(query, function (err, r, f) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        results = {
+            results: r,
+            table: t
+        };
+        console.log(results);
+        res.render("remove", {results : results});
+    });
+})
+app.post('/edit/removeEntry/Animal/confirm', urlencodedParser, function (req, res) {
+    var id = req.body.ID;
+    console.log(id);
+    mysql.pool.query("DELETE FROM Animal WHERE AnimalID = ?;", [id], function (err) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        res.sendFile(__dirname + '/confirm.html');
+    });
+})
+app.post('/edit/removeEntry/Plant/confirm', urlencodedParser, function (req, res) {
+    var id = req.body.ID;
+    console.log(id);
+    mysql.pool.query("DELETE FROM Plant WHERE PlantID = ?;", [id], function (err) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        res.sendFile(__dirname + '/confirm.html');
+    });
+})
 app.get('/edit/edit/', function (req, res) {
     res.sendFile(__dirname + '/editPages/editEntry.html');
+})
+app.post('/edit/editEntry/', urlencodedParser, function (req, res) {
+    var t = req.body.table;
+    var query = 'SELECT * FROM ' + t + ';';
+    console.log(query);
+    mysql.pool.query(query, function (err, r, f) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        results = {
+            results: r,
+            table: t
+        };
+        console.log(results);
+        res.render("edit" + t, {results : results});
+    });
+})
+app.post('/edit/editEntry/Animal/confirm', urlencodedParser, function (req, res) {
+    var data = {
+        id: req.body.ID,
+        commonName: req.body.CommonName,
+        genus: req.body.Genus,
+        species: req.body.Species,
+        subspecies: req.body.Subspecies
+    };
+    mysql.pool.query("UPDATE Animal SET CommonName = ?, Genus = ?, Species = ?, Subspecies = ? WHERE AnimalID = ?;", [data.commonName, data.genus, data.species, data.subspecies, data.id], function (err) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        res.sendFile(__dirname + '/confirm.html');
+    });
+})
+app.post('/edit/editEntry/Plant/confirm', urlencodedParser, function (req, res) {
+    var data = {
+        id: req.body.ID,
+        commonName: req.body.CommonName,
+        genus: req.body.Genus,
+        species: req.body.Species,
+        subspecies: req.body.Subspecies
+    };
+    mysql.pool.query("UPDATE Plant SET CommonName = ?, Genus = ?, Species = ?, Subspecies = ? WHERE PlantID = ?;", [data.commonName, data.genus, data.species, data.subspecies, data.id], function (err) {
+        if(err) res.sendFile(__dirname + '/error.html');
+        res.sendFile(__dirname + '/confirm.html');
+    });
 })
 app.get('/search/', function (req, res) {
     res.sendFile(__dirname + '/search.html');
